a299166efb3d449a8821e89cb990e28e1a70ae7e
P-UKD <118949473+P-UKD@users.noreply.github.com>
2024-04-01 20:57:56 +0300

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -2,8 +2,8 @@ x = int(input())
 y = int(input())
 
 if x != y:
-    x = (x + y) / 2 if x < y else x * y * 2
-    y = (x + y) / 2 if y < x else x * y * 2
-    print(x, y)
+    x1 = (x + y) / 2 if x < y else x * y * 2
+    y1 = (x + y) / 2 if y < x else x * y * 2
+    print(x1, y1)
 else:
     print("Числа не повинні бути рівні одне одному")
